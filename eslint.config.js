7bd7f9cb5c4c248889e31ae14641786cb23de817
Tim Landolt <111227353+timlandolt@users.noreply.github.com>
2026-05-06 12:04:00 +0200

    Setup project (#1)

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ["app/javascript/**/*.js"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  eslintConfigPrettier,
+]);
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,30 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+require "simplecov"
+SimpleCov.start "rails" do
+  # TODO: Remove filters as soon as logic is added
+  add_filter "lib/ferbe.rb"
+  add_filter "lib/ferbe/version.rb"
+  add_filter "lib/ferbe/engine.rb"
+  add_filter "app/controllers/ferbe/application_controller.rb"
+  add_filter "app/helpers/ferbe/application_helper.rb"
+  add_filter "app/jobs/ferbe/application_job.rb"
+  add_filter "app/models/ferbe/application_record.rb"
+
+  enable_coverage :branch
+  enable_coverage_for_eval
+  minimum_coverage line: 100, branch: 100
+end
+
 require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [ File.expand_path("../test/dummy/db/migrate", __dir__) ]
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 require "rails/test_help"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
-  ActiveSupport::TestCase.fixture_paths = [ File.expand_path("fixtures", __dir__) ]
+  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
   ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
