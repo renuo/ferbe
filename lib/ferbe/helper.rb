6251ae39667200eba1b63e87db13001c5fc4de9a
Tim Landolt <timlandolt@gmx.ch>
2026-06-23 15:44:42 +0200

    Remove unused helper method

--- lib/ferbe/helper.rb
+++ lib/ferbe/helper.rb
@@ -12,16 +12,6 @@ module Ferbe
 
       javascript_import_module_tag "ferbe/host"
     end
-
-    def ferbe_editor_tag
-      return unless Ferbe.configuration.enabled
-
-      tag.div class: "ferbe__editor", id: "ferbe-editor", data: {
-        modifier_key: Ferbe.configuration.modifier_key,
-        use_local_editor: Ferbe.configuration.use_local_editor,
-        turbo_permanent: true
-      }
-    end
     #:nocov:
   end
 end
