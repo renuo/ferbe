2f22c9fa35af15ca6be440b1eb25a53416a13ec6
Tim Landolt <timlandolt@gmx.ch>
2026-06-23 11:07:48 +0200

    Set display contents on template elements

--- lib/ferbe/helper.rb
+++ lib/ferbe/helper.rb
@@ -1,6 +1,11 @@
 module Ferbe
   module Helper
     #:nocov: -> tested implicitly through all manual system tests
+    def ferbe_styles_tag
+      return unless Ferbe.configuration.enabled
+
+      stylesheet_link_tag "ferbe/host", media: "all"
+    end
 
     def ferbe_javascript_tag
       return unless Ferbe.configuration.enabled
