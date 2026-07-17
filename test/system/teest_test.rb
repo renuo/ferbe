01fc5b2397500b1d324d3c1fc9e306aa788c57b2
Tim Landolt <timlandolt@gmx.ch>
2026-07-17 17:28:58 +0200

    Remove test test

--- test/system/teest_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class TeestTest < ApplicationSystemTestCase
-  test "some thing" do
-    visit root_path
-
-    assert_text "Welcome to the Homepage"
-  end
-end
