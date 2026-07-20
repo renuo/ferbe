a14994029a11357874d98fb3b099c66b38c6b4f0
Tim Landolt <timlandolt@gmx.ch>
2026-07-20 07:21:20 +0200

    Test editing

--- test/system/happy_path_test.rb
+++ test/system/happy_path_test.rb
@@ -4,14 +4,33 @@ require "application_system_test_case"
 
 class HappyPathTest < ApplicationSystemTestCase
   test "opening editor" do
-    visit root_path
-
-    find(".grid .red").click(:alt)
+    open_template
 
     assert_text "app/views/home/index.html.erb"
     assert_text "app/views/shared/_grid.html.erb"
     assert_text "app/views/shared/colors/_red.html.erb"
 
     assert_text '<div class="partial red"></div>'
+  end
+
+  test "modify template" do
+    open_template
+
+    input = find(".hljs-string").native
+    page.driver.browser.action
+        .move_to(input)
+        .double_click
+        .click
+        .perform
+    input.send_keys "<div>MODIFIED TEMPLATE</div>"
+    click_button "commit"
+  end
+
+  private
+
+  def open_template
+    visit root_path
+
+    find(".grid .red").click(:alt)
   end
 end
