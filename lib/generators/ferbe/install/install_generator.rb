6f2d8b81d8db371b9bba7d8e33e523a1fe275110
Tim Landolt <timlandolt@gmx.ch>
2026-07-22 14:29:39 +0200

    Add tags in layout

--- lib/generators/ferbe/install/install_generator.rb
+++ lib/generators/ferbe/install/install_generator.rb
@@ -8,4 +8,16 @@ class Ferbe::InstallGenerator < Rails::Generators::Base
   def copy_initializer
     copy_file "initializer.rb", "config/initializers/ferbe.rb"
   end
+
+  def add_tags_to_layout
+    application_layout = Rails.root.join("app/views/layouts/application.html.erb")
+    tag = "\n\n  <%= ferbe_tags %>"
+
+    if application_layout.exist?
+      insert_into_file application_layout.to_s, tag, before: /\s*<\/head>/
+    else
+      say "The default application.html.erb is missing.", :red
+      say "Add the following tag to the head section of your layout:#{tag}"
+    end
+  end
 end
